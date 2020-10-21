eac32fc9e82dcd67cc95035bc6268f21500346ee
Nuthapong <nuthapong.lert@gmail.com>
2020-10-21 17:14:17 +0700

    Node http example

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-console.log("hello world");
-console.log("change second commit")
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1> Hello world!!</h1></body></html>');
+})
+const hostname = 'localhost';
+const port = 3000;
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+});
